fdc9f8fa08852ee3a5a30a665d36a8f092d5f794
Ian Otieno Ochieng <ochiengian383@gmail.com>
2026-08-10 17:43:34 +0300

    feat: implement CodeIgniter 4 specific AccessDeniedException

--- src/Support/VimaRegistrar.php
+++ src/Support/VimaRegistrar.php
@@ -89,13 +89,13 @@ class VimaRegistrar
         $container->register(PolicyRegistryInterface::class, fn() => PolicyRegistry::instance());
 
         // Register framework specific AccessDeniedException implementation
-        CoreAccessDeniedException::useFactory(
+        \Vima\Core\Exceptions\AccessDeniedException::useFactory(
             fn(string $permission, mixed $user = null, mixed $userResolver = null) =>
-            AccessDeniedException::forPermission($permission, $user, $userResolver)
+            \Vima\CodeIgniter\Exceptions\AccessDeniedException::forPermission($permission, $user, $userResolver)
         );
 
-        if (class_exists(Services::class)) {
-            Services::injectMock('vima', $container->get(AuthorizationService::class));
+        if (class_exists(\Config\Services::class)) {
+            \Config\Services::injectMock('vima', $container->get(AuthorizationService::class));
         }
 
         Discovery::discoverPolicies();
